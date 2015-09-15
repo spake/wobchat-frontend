dd20af5222bf911719da9faa88515368e9bdaa7d
John Wiseheart <johnwiseheart@gmail.com>
2015-09-16 00:56:32 +1000

    Removed all traces of WobChat.jsx

--- react_components/App.jsx
+++ react_components/App.jsx
@@ -1,7 +1,6 @@
 var React = require('react'),
     Router = require('./Router.jsx');
-var WobChat = require('./WobChat.jsx');
-var injectTapEventPlugin = require("react-tap-event-plugin");
+let injectTapEventPlugin = require("react-tap-event-plugin");
  
 //Needed for onTouchTap 
 //Can go away when react 1.0 release 
