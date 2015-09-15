4a2e8ba7d657ef0bc593742a801f044d3d6db30b
John Wiseheart <johnwiseheart@gmail.com>
2015-09-15 23:33:46 +1000

    Found a good library

--- react_components/WobChat.jsx
+++ react_components/WobChat.jsx
@@ -4,10 +4,8 @@ var React = require('react'),
     RaisedButton = mui.RaisedButton,
     AppBar       = mui.AppBar,
     FriendsList  = require('./FriendsList.jsx'),
-    SignIn       = require('./SignIn.jsx'),
-    SignOut      = require('./SignOut.jsx'),
     PurpleTheme  = require('./PurpleTheme.jsx');
-
+var googleApiLoader = require('./GAPI.jsx');
 ThemeManager.setPalette(PurpleTheme);
 module.exports = React.createClass({
     childContextTypes: {
@@ -24,22 +22,65 @@ module.exports = React.createClass({
     },
     componentDidMount: function() {
         var self = this;
+        var _this = this;
+
+        googleApiLoader.authLoaded(function () {
+            _this.setState({authLoaded: true});
+
+            googleApiLoader.getAuth2().currentUser.listen(function (user) {
+                _this.setState({finishedLoading: true});
+                if (user.getBasicProfile()) {
+                    var profile = user.getBasicProfile();
+                    var profileProxy = {};
+                    profileProxy.id = profile.getId();
+                    profileProxy.name = profile.getName();
+                    profileProxy.thumb = profile.getImageUrl();
+                    profileProxy.email = profile.getEmail();
+                    _this.setState({loggedInUser: profileProxy});
+                }
+                _this.setState({isLoggedIn: user.getBasicProfile() ? true : false});
+            });
+        });
+
+        googleApiLoader.clientsLoaded(function () {
+            _this.setState({clientsLoaded: true});
+        });
+    },
+    toggleSignIn: function () {
+        if (!googleApiLoader.getAuth2().isSignedIn.get()) {
+            googleApiLoader.signIn();
+        } else {
+            googleApiLoader.signOut();
+        }
     },
     openChat: function(element, event) {
         console.log("We should probably open a chat here to " + element.props.user.name)
     },
     render: function() {
-        return (
-            <div>
-                <AppBar
-                    title="WobChat"
-                    showMenuIconButton={false}
-                    iconClassNameRight="muidocs-icon-navigation-expand-more"
-                    style={{zIndex: 20}}/>
-                <SignIn />
-                <SignOut />
-                <FriendsList openChat={this.openChat}/>
-            </div>
-        );
+        var loggedInUserThumb = null;
+
+        if (this.state.loggedInUser)
+            loggedInUserThumb = <img src={this.state.loggedInUser.thumb} />
+
+        var toggleLoginButton = <button onClick={this.toggleSignIn}>Login to Google</button>
+
+        if (this.state.finishedLoading) {
+
+            if (this.state.isLoggedIn) {
+
+                return <div>
+                {loggedInUserThumb}
+                {this.state.loggedInUser.name}
+                {toggleLoginButton}
+                    <hr />
+                    You're now free to use the Google APIs!
+                </div>
+            }
+            else
+                return toggleLoginButton;
+        }
+        else {
+            return <div>Loading...</div>
+        }
     }
 });
