432e96d631066af89a2b0c999856cab01c3e6a3e
John Wiseheart <johnwiseheart@gmail.com>
2015-09-24 23:27:44 +1000

    Moved GAPI and Config to libs, removed all traces of localSTorage

--- app/components/Config.jsx
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    apiBaseUrl: "https://api.wob.chat"
-}
---
+++ app/libs/Config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    apiBaseUrl: "https://api.wob.chat",
+    client_id: '783279836221-m71iri9830ptguifn0apfbsnj22pfeel.apps.googleusercontent.com'
+}
