9a104a9fb59a31fe5297274b466dc9ff8fc57d63
Joshua Elliott <joshuaelliott@outlook.com>
2015-09-25 23:24:24 +1000

    Added beginng of modal for confirming friend removal

--- app/components/Friend.jsx
+++ app/components/Friend.jsx
@@ -2,24 +2,48 @@ import React from 'react';
 import {ListItem, Avatar} from 'material-ui';
 import {IconMenu, MenuItem, IconButton} from 'material-ui';
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import DeleteFriendModal from './DeleteFriendModal.jsx'
+let injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
 
 class Friend extends React.Component {
   constructor(props) {
     super(props);
+
+    this.launchModal = this.launchModal.bind(this)
+    this.deleteFriend = this.deleteFriend.bind(this)
+    this.cancelDelete = this.cancelDelete.bind(this)
+  }
+
+  launchModal(ev, item) {
+    console.log(ev);
+    console.log(item);
+    console.log("EY");
+    this.refs.modal.show();
+  }
+
+  deleteFriend() {
+    console.log('delete friend');
+  }
+
+  cancelDelete() {
+    console.log('cancel delete friend'); 
   }
 
   render() {
     let user = this.props.user;
+
+    // Make IconMenu for removing friends, and perhaps other features
     let iconButton = 
         <IconButton>
             <MoreVertIcon />
-        </IconButton>
+        </IconButton>;
     let rightIconButton = 
         <IconMenu iconButtonElement={iconButton}>
-            <MenuItem index={0} primaryText="Remove Friend" />
-            <MenuItem index={1} primaryText="Potato" />
-        </IconMenu>
+            <MenuItem index={0} onClick={this.launchModal}>Remove</MenuItem>
+            <DeleteFriendModal ref='modal' user={user} deleteFriend={this.deleteFriend} cancelDelete={this.cancelDelete} />
+        </IconMenu>;
 
     return (
         <ListItem
