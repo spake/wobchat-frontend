ef2774861622072a8fe65bba83c588f6da5dee90
Joshua Elliott <joshuaelliott@outlook.com>
2015-09-25 20:24:02 +1000

    Added icon menu for friends but menu does not open

--- app/components/Friend.jsx
+++ app/components/Friend.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import {ListItem, Avatar} from 'material-ui';
+import {IconMenu, MenuItem, IconButton} from 'material-ui';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+
 
 class Friend extends React.Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     let user = this.props.user;
+    let iconButton = 
+        <IconButton>
+            <MoreVertIcon />
+        </IconButton>
+    let rightIconButton = 
+        <IconMenu iconButtonElement={iconButton}>
+            <MenuItem index={0} primaryText="Remove Friend" />
+            <MenuItem index={1} primaryText="Potato" />
+        </IconMenu>
+
     return (
         <ListItem
             key={user.uid}
             leftAvatar={<Avatar src={user.picture} />}
             primaryText={user.name}
-            onClick={this.props.onClick}/>
+            onClick={this.props.onClick}
+            rightIconButton={rightIconButton}
+        />
     );
   }
 }
