171dec04a15745a4a768428c5886538e5d8c391d
Joshua Elliott <joshuaelliott@outlook.com>
2015-10-03 05:09:56 +1000

    Forget to add User component in last commit. Added tests and removed unneeded tests.

---
+++ app/components/User.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {ListItem, Avatar} from 'material-ui';
+import {IconMenu, MenuItem, IconButton} from 'material-ui';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import FriendActions from '../actions/FriendActions';
+import FriendStore from '../stores/FriendStore';
+let injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
+
+class User extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleMenuItemClicked = this.handleMenuItemClicked.bind(this)
+    }
+
+    handleMenuItemClicked(ev, item) {
+        // Handle all menu item clicks for IconMenu here
+        // item.props.children == text in menu item
+        if (item.props.children == "Add") {
+            FriendActions.add(this.props.user.id)
+        }
+    }
+
+    render() {
+        let user = this.props.user;
+
+        // Make IconMenu for adding friends, and perhaps other features
+        let iconButton = 
+            <IconButton>
+                <MoreVertIcon />
+            </IconButton>;
+        let rightIconButton = 
+            <IconMenu iconButtonElement={iconButton} onItemTouchTap={this.handleMenuItemClicked}>
+                <MenuItem index={0}>Add</MenuItem>
+            </IconMenu>;
+
+        return (
+            <ListItem
+                key={user.uid}
+                leftAvatar={<Avatar src={user.picture} />}
+                primaryText={user.name}
+                rightIconButton={rightIconButton}
+            />
+    );
+  }
+}
+
+User.defaultProps = {
+    user: {
+        name: "John Wiseheart",
+        id: 1,
+        picture: ''
+    },
+    onClick: function() {}
+}
+
+module.exports = User;
---
+++ test/component/User.js
@@ -0,0 +1,36 @@
+var React = require('react/addons'),
+    assert = require('assert'),
+    TestUtils = React.addons.TestUtils,
+    mui = require('material-ui'),
+    User = require('../../app/components/User.jsx'),
+    ListItem = mui.ListItem,
+    Avatar = mui.Avatar,
+    IconButton = mui.IconButton,
+    IconMenu = mui.IconMenu,
+    MenuItem = mui.MenuItem;
+
+describe('Friend', function(){
+  before('testing', function() {
+    var shallowRenderer = TestUtils.createRenderer();
+    shallowRenderer.render(React.createElement(User));
+    this.component = shallowRenderer.getRenderOutput();
+  });
+
+  it('should render a ListItem', function() {
+    assert(this.component.type == ListItem);
+  });
+
+  it('should have a ListItem with a leftAvatar of type Avatar rendered', function() {
+    assert(this.component.props.leftAvatar.type == Avatar);
+  });
+
+  it('should have a ListItem with an IconMenu rendered', function() {
+    assert(this.component.props.rightIconButton.type == IconMenu);
+  });
+
+  it('should have that IconMenu with a MenuItem inside it', function() {
+    assert(this.component.props.rightIconButton.props.children.type == MenuItem);
+  });
+
+
+});
