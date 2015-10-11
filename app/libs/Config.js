bd37170d82af1389088bf1d51fd8f76084dbd314
John Wiseheart <johnwiseheart@gmail.com>
2015-10-11 21:29:24 +1100

    Add mock for local storage

--- app/libs/Config.js
+++ app/libs/Config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    apiBaseUrl: window.localStorage.apiBaseUrl || "https://api.wob.chat",
+    apiBaseUrl: localStorage.apiBaseUrl || "https://api.wob.chat",
     client_id: '783279836221-m71iri9830ptguifn0apfbsnj22pfeel.apps.googleusercontent.com'
 }
