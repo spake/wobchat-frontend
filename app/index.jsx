f17d920a5506fb04bdd10819cf00cf8e06e591ec
John Wiseheart <johnwiseheart@gmail.com>
2015-10-20 14:36:34 +1100

    Remove importing unneeded files

--- app/index.jsx
+++ app/index.jsx
@@ -2,8 +2,6 @@ import 'array.prototype.findindex';
 import React from 'react';
 import Router from './components/Router.jsx';
 import alt from './libs/alt';
-import storage from './libs/storage';
-import persist from './libs/persist';
 
 main();
 
